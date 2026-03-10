8abc6cbaf3a302f2df861d86ddc4ea0bb2b2af99
hsm <1161237802@qq.com>
2026-03-10 11:56:43 +0800

    concurrent mode

--- src/render.js
+++ src/render.js
@@ -10,11 +10,30 @@ function render(element, container) {
     .filter((key) => key !== "children")
     .forEach((key) => (dom[key] = element.props[key]));
 
-  //递归渲染子元素
-  element.props.children.forEach((child) => render(child, dom));
-
   //追加到父节点
   container.append(dom);
 }
+let nextUnitofWork = null;
+
+// 调度函数
+function workLoop(deadline) {
+  // 应该退出
+  let shouldYield = false;
+  // 有工作且不应该退出
+  while (nextUnitofWork && !shouldYield) {
+    // 做工作
+    nextUnitofWork = performUnitOfWork(nextUnitofWork);
+    // 看看还有没有足够的时间
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  // 没有足够的时间，请求下一次浏览器空闲的时候执行
+  requestIdleCallback(workLoop);
+}
+
+// 第一次请求
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(work) {}
 
 export default render;
